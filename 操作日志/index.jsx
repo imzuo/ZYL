03177f30a00974e5b6866a2f25bc2af67735f6c5
ZuoYongLai <2207314301@qq.com>
2022-02-27 22:15:40 +0800

    更新

--- 操作日志/index.jsx
+++ 操作日志/index.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import {Timeline,Card} from 'antd'
+import {Timeline,Card,DatePicker} from 'antd'
+import Mock from 'mockjs'
+import moment from 'moment';
 import 'antd/dist/antd.css'
 import styles from './index.css'
-import Mock from 'mockjs'
+
 
 //生成模拟数据
 let dataSource = Mock.mock({
@@ -16,10 +18,14 @@ let dataSource = Mock.mock({
   }]
 })
 
+const {RangePicker} = DatePicker;
+const dateFormat = 'MM-DD';
+
 //排列数组
 let data = Array.from(dataSource.dataSources);
 
 let sortedData =data.sort((b, a) => a.date.localeCompare(b.date) || a.time.localeCompare(b.time));
+
 
 console.log({dataSource});
 console.log({data});
@@ -31,6 +37,14 @@ const itemColor=(action)=>{
   action==='新建'? "green":action==='删除'? "red":action==='修改'? "blue":"blue"
   )
 }
+//按照时间筛选
+function onDateChange(date, dateString) {
+  console.log({date,dateString});
+  let filtedData = sortedData.filter((logs) =>{
+    return  "05-05"<logs.date<"10-10"
+  })
+  console.log({filtedData})
+}
 
 
 class Demo extends Component {
@@ -38,12 +52,26 @@ class Demo extends Component {
   render() {
     return (
       <div>
-        <h1 style={{ marginLeft: 300,marginTop:20}}>操作日志</h1>{/*标题*/}
-        <Card className={styles.card}>
+        {/*标题*/}
+        <h1 style={{ marginLeft: 300,marginTop:20}}>操作日志</h1>
+        <br/>
+        <hr style={{height:1 ,}} />
+        {/*卡片*/}
+        <Card className={styles.card} title="index">
           <p>红色圈代表删除</p>
           <p>绿色圈代表新建</p>
           <p>蓝色圈代表修改和审阅</p>
         </Card>
+
+        {/*日期选择*/}
+        <p className={styles.dataPicker2}>按时间筛选：</p>
+        <RangePicker
+          className={styles.dataPicker}
+          onChange={onDateChange}
+          defaultValue={[moment('01-01', dateFormat), moment('12-31', dateFormat)]}
+          format={dateFormat}
+        />
+        {/*主体*/}
         <div className={styles.body}>
            <Timeline>
             {
